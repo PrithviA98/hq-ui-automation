3e66bf843206556b67fac4c56b7e9ea2aa03b9f8
Prithvi <prithvia98@gmail.com>
2026-01-20 00:24:08 +0530

    hq-automation: Added signup script in package.json and test script for Login

---
+++ tests/test-signup.spec.ts
@@ -0,0 +1,30 @@
+const config = require('./src/resources/config.json');
+const testData = require('./src/resources/testdata.json');
+
+import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/HomePage.ts';
+
+
+
+test.describe("Login and Verify Book", () => {
+  let homePage: HomePage;
+  let uiUrl: string;
+
+  test.beforeEach(async ({ page }) => {
+    uiUrl = config["testUrl"];
+    homePage = new HomePage(page);
+
+  });
+
+  test('Signup and Verify Success', async () => {
+    await homePage.launchPage(uiUrl);
+    console.log("Launched URL: " + uiUrl);
+    await homePage.enterName(testData["name"]);
+    await homePage.enterEmail(testData["email"]);
+    await homePage.clickSignUp();
+
+  })
+
+
+
+});
