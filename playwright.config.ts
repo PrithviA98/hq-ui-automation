6eb417d5debdfcf83c67613c7492558af87fd26f
Prithvi <prithvia98@gmail.com>
2026-01-20 20:04:48 +0530

    hq-automation: Updated playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
     video: 'retain-on-failure'
   },
